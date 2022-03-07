8367f6b8b715d2b53a8a59c6670badf8c04333cb
ervin <1119427810@qq.com>
2022-03-07 21:14:03 +0800

    链表反转，遍历实现

---
+++ src/main/java/com/cjervin/arithmetic/arithmetic/ReverseList.java
@@ -0,0 +1,54 @@
+package com.cjervin.arithmetic.arithmetic;
+
+
+/**
+ * 链表反转 两种方法 1：遍历 2：递归
+ *
+ * @author ervin
+ * @Date 2022/3/7
+ */
+public class ReverseList {
+
+
+    private static class Node<T> {
+        T item;
+        Node next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+
+    /**
+     * 遍历方式
+     * @param node
+     * @return
+     */
+    public static Node iterate(Node node) {
+        Node pre = null;
+        Node next;
+        Node current = node;
+        while (current != null) {
+            next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+
+        return current;
+    }
+
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1, null);
+        Node node2 = new Node(2, node1);
+        Node node3 = new Node(3, node2);
+        Node node4 = new Node(4, node3);
+        Node node5 = new Node(5, node4);
+
+        Node iterate = iterate(node5);
+        System.out.println("=====");
+    }
+}
