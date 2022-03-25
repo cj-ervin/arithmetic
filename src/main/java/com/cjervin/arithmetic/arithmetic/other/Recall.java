5c4a52d9b8f60a1e9677331da3c841aedd979833
ervin <1119427810@qq.com>
2022-03-25 20:21:32 +0800

    回溯算法--子集

--- src/main/java/com/cjervin/arithmetic/arithmetic/other/Recall.java
+++ src/main/java/com/cjervin/arithmetic/arithmetic/other/Recall.java
@@ -56,4 +56,43 @@ public class Recall {
 
     }
 
+
+    // 记录回溯算法的递归路径
+    LinkedList<Integer> track = new LinkedList<>();
+
+    /**
+     * 子集
+     * <p>
+     * https://leetcode-cn.com/problems/subsets/solution/zi-ji-by-changxiaojie-idd4/
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+
+    /**
+     * 回溯算法核心函数，遍历子集问题的回溯树
+     *
+     * @param nums
+     * @param start
+     */
+    void backtrack(int[] nums, int start) {
+
+        // 前序位置，每个节点的值都是一个子集
+        res.add(new LinkedList<>(track));
+
+        // 回溯算法标准框架
+        for (int i = start; i < nums.length; i++) {
+            // 做选择
+            track.addLast(nums[i]);
+            // 通过 start 参数控制树枝的遍历，避免产生重复的子集
+            backtrack(nums, i + 1);
+            // 撤销选择
+            track.removeLast();
+        }
+    }
+
 }
